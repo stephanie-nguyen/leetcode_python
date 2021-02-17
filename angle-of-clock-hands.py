0bca4cee186f07b865136d44ba5726c31462b33a
stephanie nguyen <47327419+stephanie-nguyen@users.noreply.github.com>
2021-02-17 13:44:23 -0600

    Update angle-of-clock-hands.py

--- angle-of-clock-hands.py
+++ angle-of-clock-hands.py
@@ -1,3 +1,7 @@
+'''
+Given two numbers, hour and minutes. Return the smaller angle (in degrees) formed between the hour and the minute hand.
+'''
+
 def calcAngle(hour,minute): 
         if (hour < 0 or minute < 0 or hour > 12 or minute > 60): #check for valid time
             print('Enter valid time') 
@@ -13,6 +17,7 @@ def calcAngle(hour,minute):
         angle = abs(hour_angle - minute_angle) #12 is 0deg. find diff& angle btwn
         smallerangle = min(360 - angle, angle) #gives smaller angle being cut
         return smallerangle 
+
 hour = 9
 minute = 4
 print(calcAngle(hour,minute), 'degrees') 
