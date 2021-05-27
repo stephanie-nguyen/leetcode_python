38aa88a23512a4112b2e4e646cd9e0cbdf2d10fd
Ni Nguyen <ni@Nis-MacBook-Pro.attlocal.net>
2021-05-26 22:10:46 -0500

    update

--- virtu-appleweight.py
+++ virtu-appleweight.py
@@ -5,12 +5,10 @@ apples to that box until it is full.
 Write a function:
     def solution(A)
 that, given a zero-indexed array A consisting of N integers, representing the weight of items alreay in the box and each 
-\apple's weight, returns the maximum 
-number of apples that could fit in the box, without exceeding its capacity.
+apple's weight, returns the maximum number of apples that could fit in the box, without exceeding its capacity.
 
 The input array consists of an integer K as the first element, representing the sum of the weights of items already contained 
-in the box followed by zero or more 
-integers representing individual apple weights.
+in the box followed by zero or more integers representing individual apple weights.
 
 You can assume that A contains between 1 and 100 elements and that every number in it is >= 0 and <= 5000.
 Note that an apple can weigh 0 grams, and that you should maximize the number of apples in the box, not their total weight.
@@ -18,17 +16,18 @@ Note that an apple can weigh 0 grams, and that you should maximize the number of
 For example, for an input of:
 [4650, 150, 150, 150]
 
-You should return 2, as the box already contains K=4650 grams of items, so only 2 more apples of weight 150 wiykd fit 
-(bringing the total weight to 4950, still 
-below the capacity).
+You should return 2, as the box already contains K=4650 grams of items, so only 2 more apples of weight 150 would fit 
+(bringing the total weight to 4950, still  below the capacity).
 
 For an input of: 
 [4850, 100, 30, 30, 100, 50, 100]
 You should return 3, as you could put in two 30-gram apples and the 50-gram apple.
 '''
 
+import pdb
 def solution(A):
     remaining = 5000-A[0]
+    pdb.set_trace()
     apples=A[1:]
     apples.sort()
     count=0
